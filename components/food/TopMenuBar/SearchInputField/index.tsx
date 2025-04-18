cc3bce83a49807386d82a9e7454017adf0dae314
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-04-19 01:02:39 +0530

    Update SearchInputField styling for improved layout and spacing

--- components/food/TopMenuBar/SearchInputField/index.tsx
+++ components/food/TopMenuBar/SearchInputField/index.tsx
@@ -5,7 +5,7 @@ import { View, Text } from "react-native";
 
 const SearchInputField = () => {
   return (
-    <View className="flex flex-row border w-full border-gray-400 rounded-3xl gap-4 p-2 py-3 items-center justify-between">
+    <View className="flex flex-row border w-full border-gray-400 rounded-3xl gap-4 p-3 py-5 items-center justify-between">
       <View className="gap-2 px-2 flex flex-row items-center border-r w-11/12 border-gray-400 ">
         <Ionicons name="search" size={20} color={"#9ca3af"} className="" />
         <Text className="text-md text-gray-400">Search for 'Sweets'</Text>
