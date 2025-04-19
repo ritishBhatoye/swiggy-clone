b2548b2a8a49bd0403c1a3393c43ac7eada91bdc
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-04-19 13:54:05 +0530

    carousel imported successfully

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,8 @@
-// import FoodCarousel from "@/components/food/Carousel";
+import Card from "@/components/elements/Card";
+import FoodCarousel from "@/components/food/Carousel";
 import TopMenuBar from "@/components/food/TopMenuBar";
+import { CarouselData } from "@/constants/dummyData/CardCarouselData";
+import { CarouselCardType } from "@/types";
 import { ScrollView, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -8,7 +11,13 @@ export default function HomeScreen() {
     <SafeAreaView className="bg-white">
       <View className="w-11/12 mx-auto px-2">
         <TopMenuBar />
-        <ScrollView>{/* <FoodCarousel /> */}</ScrollView>
+        <ScrollView>
+          <FoodCarousel
+            card={(food: CarouselCardType) => (
+              <Card key={food.id} item={food} />
+            )}
+          />
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
