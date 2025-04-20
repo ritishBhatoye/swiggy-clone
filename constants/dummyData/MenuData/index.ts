47a4e5008294a63f4e4b3cd81cc5e3ca60a8333c
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-04-20 11:39:07 +0530

    refactor: update image sources in MenuCard and MenuData to use local assets

--- constants/dummyData/MenuData/index.ts
+++ constants/dummyData/MenuData/index.ts
@@ -3,122 +3,102 @@ import { MenuCardDataType } from "@/types";
 export const MenuData: MenuCardDataType[] = [
   {
     id: 1,
-    image:
-      "https://www.freepnglogos.com/uploads/pizza-png/pizza-png-pizza-images-download-0.png",
+    image: "@/assets/images/home/menu/pizza.png",
     title: "Pizza",
   },
   {
     id: 2,
-    image:
-      "https://www.freepnglogos.com/uploads/cake-png/cake-png-transparent-cake-images-pluspng-21.png",
+    image: "@/assets/images/home/menu/rashmalai.png",
     title: "Rashmalai",
   },
   {
     id: 3,
-    image:
-      "https://www.freepnglogos.com/uploads/ice-cream-png/ice-cream-png-ice-cream-cone-png-transparent-ice-cream-cone-images-9.png",
+    image: "@/assets/images/home/menu/ice-cream.png",
     title: "Ice Cream",
   },
   {
     id: 4,
-    image:
-      "https://www.freepnglogos.com/uploads/momos-png/momos-png-images-download-8.png",
+    image: "@/assets/images/home/menu/momos.png",
     title: "Momos",
   },
   {
     id: 5,
-    image:
-      "https://www.freepnglogos.com/uploads/pasta-png/pasta-png-images-transparent-background-png-arts-19.png",
+    image: "@/assets/images/home/menu/pasta.png",
     title: "Pasta",
   },
   {
     id: 6,
-    image:
-      "https://www.freepnglogos.com/uploads/idli-png/idli-png-images-download-png-arts-4.png",
+    image: "@/assets/images/home/menu/idli.png",
     title: "Idli",
   },
   {
     id: 7,
-    image:
-      "https://www.freepnglogos.com/uploads/noodles-png/noodles-png-noodle-transparent-png-images-pluspng-26.png",
+    image: "@/assets/images/home/menu/chinese.png",
     title: "Chinese",
   },
   {
     id: 8,
-    image:
-      "https://www.freepnglogos.com/uploads/coffee-png/coffee-png-coffee-cup-download-png-14.png",
+    image: "@/assets/images/home/menu/coffee.png",
     title: "Coffee",
   },
   {
     id: 9,
-    image:
-      "https://www.freepnglogos.com/uploads/juice-png/juice-png-orange-juice-png-transparent-orange-juice-images-30.png",
+    image: "@/assets/images/home/menu/juice.png",
     title: "Juice",
   },
   {
     id: 10,
-    image:
-      "https://www.freepnglogos.com/uploads/burger-png/burger-png-burger-png-images-download-19.png",
+    image: "@/assets/images/home/menu/burger.png",
     title: "Burger",
   },
   {
     id: 11,
-    image:
-      "https://www.freepnglogos.com/uploads/noodles-png/noodles-png-images-download-png-arts-24.png",
+    image: "@/assets/images/home/menu/noodles.png",
     title: "Noodles",
   },
   {
     id: 12,
-    image:
-      "https://www.freepnglogos.com/uploads/thali-png/thali-png-indian-thali-transparent-png-images-pluspng-0.png",
+    image: "@/assets/images/home/menu/north-indian.png",
     title: "North Indian",
   },
   {
     id: 13,
-    image:
-      "https://www.freepnglogos.com/uploads/dosa-png/dosa-png-transparent-dosa-images-5.png",
+    image: "@/assets/images/home/menu/dosa.png",
     title: "Dosa",
   },
   {
     id: 14,
-    image:
-      "https://www.freepnglogos.com/uploads/salad-png/salad-png-images-download-png-arts-14.png",
+    image: "@/assets/images/home/menu/pure-veg.png",
     title: "Pure Veg",
   },
   {
     id: 15,
-    image:
-      "https://www.freepnglogos.com/uploads/paratha-png/paratha-png-transparent-paratha-images-3.png",
+    image: "@/assets/images/home/menu/paratha.png",
     title: "Paratha",
   },
   {
     id: 16,
-    image:
-      "https://www.freepnglogos.com/uploads/tea-png/tea-png-transparent-tea-images-pluspng-21.png",
+    image: "@/assets/images/home/menu/tea.png",
     title: "Tea",
   },
   {
     id: 17,
-    image:
-      "https://www.freepnglogos.com/uploads/salad-png/salad-png-transparent-images-png-only-0.png",
+    image: "@/assets/images/home/menu/salad.png",
     title: "Salad",
   },
   {
     id: 18,
-    image:
-      "https://www.freepnglogos.com/uploads/thali-png/thali-png-indian-thali-transparent-png-images-2.png",
+    image: "@/assets/images/home/menu/south-indian.png",
     title: "South Indian",
   },
   {
     id: 19,
-    image:
-      "https://www.freepnglogos.com/uploads/pancake-png/pancake-png-images-download-png-arts-16.png",
+    image: "@/assets/images/home/menu/pancake.png",
     title: "Pancake",
   },
   {
     id: 20,
-    image:
-      "https://www.freepnglogos.com/uploads/biryani-png/biryani-png-biryani-images-download-png-arts-4.png",
+    image: "@/assets/images/home/menu/biryani.png",
     title: "Biryani",
   },
 ];
