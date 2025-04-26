e4e35a4b073333ce4ed294133f7f2e9a9f288e83
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-04-26 14:26:37 +0530

    feat: implement new filtering logic in MenuSection for improved data handling

---
+++ constants/dummyData/Filters/index.ts
@@ -0,0 +1,24 @@
+import { FilterDataType } from "@/types";
+
+export const FiltersOptionsData: FilterDataType[] = [
+  {
+    id: 1,
+    label: "Filters",
+  },
+  {
+    id: 2,
+    label: "Sort By",
+  },
+  {
+    id: 3,
+    label: "Pure Veg",
+  },
+  {
+    id: 4,
+    label: "Ratings 4.0+",
+  },
+  {
+    id: 5,
+    label: "Ratings 4.5+",
+  },
+];
