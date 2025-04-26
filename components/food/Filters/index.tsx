aa94947eca5fe669560b11f0566931638f6a64a3
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-04-26 14:30:31 +0530

    feat: enhance Filters component to dynamically render filter options from data

--- components/food/Filters/index.tsx
+++ components/food/Filters/index.tsx
@@ -1,7 +1,16 @@
+import { Filter } from "@/components/elements/Filter";
+import { FiltersOptionsData } from "@/constants/dummyData/Filters";
+import { FilterDataType } from "@/types";
 import React from "react";
 
 import { View } from "react-native";
 
 export default function Filters() {
-  return <View>Filters</View>;
+  return (
+    <View className="w-full">
+      {FiltersOptionsData.map((filter: FilterDataType) => (
+        <Filter key={filter.id} filter={filter} />
+      ))}
+    </View>
+  );
 }
