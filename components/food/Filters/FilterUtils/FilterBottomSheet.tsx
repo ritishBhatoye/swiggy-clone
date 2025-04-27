2c71dc4b7b1e2afbab2dc2ba30592f1a2e859097
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-04-27 12:48:14 +0530

    feat: add react-native-radio-buttons-group dependency and update SortByBottom component to use it for sorting options

--- components/food/Filters/FilterUtils/FilterBottomSheet.tsx
+++ components/food/Filters/FilterUtils/FilterBottomSheet.tsx
@@ -1,28 +1,32 @@
-import React from "react";
-import { TouchableOpacity, View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text } from "react-native";
 import ActionSheet from "react-native-actions-sheet";
-import RadioButtonGroup, { RadioButtonItem } from "expo-radio-button";
+import RadioGroup from "react-native-radio-buttons-group";
 
 export const SortByBottom = ({
   ref,
 }: {
   ref: React.MutableRefObject<null>;
 }) => {
-  const [selected, setSelected] = React.useState("");
+  const [selectedId, setSelectedId] = useState("lowToHigh");
+
+  const radioButtons = [
+    { id: "lowToHigh", label: "Price: Low to High", value: "lowToHigh" },
+    { id: "highToLow", label: "Price: High to Low", value: "highToLow" },
+    { id: "newest", label: "Newest First", value: "newest" },
+    { id: "oldest", label: "Oldest First", value: "oldest" },
+  ];
 
   return (
     <ActionSheet ref={ref}>
-      <View className="flex flex-col items-start p-4">
-        <Text className="font-medium text-gray-400 text-sm mb-4">SORT BY</Text>
-        <RadioButtonGroup
-          selected={selected}
-          onSelected={(value) => setSelected(value)}
-          radioBackground="black"
-        >
-          <RadioButtonItem value="Option 1" label="Option 1" />
-          <RadioButtonItem value="Option 2" label="Option 2" />
-          <RadioButtonItem value="Option 3" label="Option 3" />
-        </RadioButtonGroup>
+      <View className="p-4">
+        <Text className="font-medium text-gray-400 text-sm mb-2">SORT BY</Text>
+        <RadioGroup
+          radioButtons={radioButtons}
+          onPress={setSelectedId}
+          selectedId={selectedId}
+          containerStyle={{ flexDirection: "column" }}
+        />
       </View>
     </ActionSheet>
   );
