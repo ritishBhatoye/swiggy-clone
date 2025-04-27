cfee67604b8355861bac5bc430bd3ff15bdacb28
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-04-27 12:45:54 +0530

    refactor: remove expo-radio-button dependency from package.json and package-lock.json

--- components/food/Filters/FilterUtils/FilterBottomSheet.tsx
+++ components/food/Filters/FilterUtils/FilterBottomSheet.tsx
@@ -1,21 +1,28 @@
 import React from "react";
-
 import { TouchableOpacity, View, Text } from "react-native";
 import ActionSheet from "react-native-actions-sheet";
+import RadioButtonGroup, { RadioButtonItem } from "expo-radio-button";
 
 export const SortByBottom = ({
   ref,
 }: {
   ref: React.MutableRefObject<null>;
 }) => {
+  const [selected, setSelected] = React.useState("");
+
   return (
     <ActionSheet ref={ref}>
-      <View className="grid grid-cols-2">
-        <View></View>
-        <View className="flex flex-col items-start">
-          <Text className="font-medium text-gray-400 text-sm">SORT BY</Text>
-          <View></View>
-        </View>
+      <View className="flex flex-col items-start p-4">
+        <Text className="font-medium text-gray-400 text-sm mb-4">SORT BY</Text>
+        <RadioButtonGroup
+          selected={selected}
+          onSelected={(value) => setSelected(value)}
+          radioBackground="black"
+        >
+          <RadioButtonItem value="Option 1" label="Option 1" />
+          <RadioButtonItem value="Option 2" label="Option 2" />
+          <RadioButtonItem value="Option 3" label="Option 3" />
+        </RadioButtonGroup>
       </View>
     </ActionSheet>
   );
