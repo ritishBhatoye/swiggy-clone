c3094b064fef5bb72a214295a44488020c6c2c17
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-04-27 12:30:57 +0530

    feat: add SORT BY header to SortByBottom component in Filters

--- components/food/Filters/FilterUtils/FilterBottomSheet.tsx
+++ components/food/Filters/FilterUtils/FilterBottomSheet.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { TouchableOpacity, View } from "react-native";
+import { TouchableOpacity, View, Text } from "react-native";
 import ActionSheet from "react-native-actions-sheet";
 
 export const SortByBottom = ({
@@ -12,6 +12,10 @@ export const SortByBottom = ({
     <ActionSheet ref={ref}>
       <View className="grid grid-cols-2">
         <View></View>
+        <View className="flex flex-col items-start">
+          <Text className="font-medium text-gray-400 text-sm">SORT BY</Text>
+          <View></View>
+        </View>
       </View>
     </ActionSheet>
   );
