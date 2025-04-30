ecff98ee9f43da619e3fb6d9248dbf5c14811376
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-04-30 20:47:42 +0530

    refactor: update SignInScreen layout to include account header and comment out mobile input field

--- screens/SignIn.tsx
+++ screens/SignIn.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import { View, Dimensions, Text } from "react-native";
 import { Image } from "expo-image";
 import InputWithLabel from "@/components/atoms/InputWithLabel";
+import Button from "@/components/atoms/Button";
 
 const { width: screenWidth } = Dimensions.get("window");
 
@@ -18,12 +19,19 @@ const SignInScreen = () => {
       />
       <View className="w-full h-full  bg-primary-500">
         <View className="rounded-t-[30px] p-8 bg-white h-full">
-          <InputWithLabel
+          <View className="flex flex-col items-center ">
+            <Text className="text-2xl text-black font-medium">ACCOUNT</Text>
+            <Text className="text-sm text-gray-400 font-medium">
+              Login/Create Account to manage order
+            </Text>
+            <Button></Button>
+          </View>
+          {/* <InputWithLabel
             label="Mobile "
             value={mobile}
             onChangeText={setMobile}
             placeholder=""
-          />
+          /> */}
         </View>
       </View>
     </View>
