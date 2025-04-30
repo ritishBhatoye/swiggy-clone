54227b4ca81a7b9419572a0c0a19b75e4a1c21e9
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-04-30 21:03:38 +0530

    style: refine layout and typography in SignInScreen for enhanced readability and user engagement

--- screens/SignIn.tsx
+++ screens/SignIn.tsx
@@ -18,20 +18,26 @@ const SignInScreen = () => {
         style={{ width: screenWidth, height: 250 }}
       />
       <View className="w-full h-full  bg-primary-500">
-        <View className="rounded-t-[30px] p-8 bg-white h-full">
-          <View className="flex flex-col items-center w-11/12 mx-auto">
-            <Text className="text-2xl text-black font-medium text-start">
+        <View className="rounded-t-[30px] py-8 bg-white h-full">
+          <View className="flex flex-col items-center w-11/12 mx-auto gap-1">
+            <Text className="text-2xl text-black font-semibold text-start w-full">
               ACCOUNT
             </Text>
-            <Text className="text-sm text-gray-400 font-medium">
+            <Text className="text-base text-gray-400 font-medium w-full">
               Login/Create Account to manage order
             </Text>
 
             <Button
               title={"LOGIN"}
               textClassName="px-12 "
-              className="rounded-none my-5"
+              className="rounded-sm my-2 py-4 mt-3 w-full"
             />
+            <Text className="w-full text-sm  font-medium text-gray-400">
+              By clicking, I accept the{" "}
+              <Text className="text-black font-semibold">
+                Terms & Conditions & Privacy
+              </Text>
+            </Text>
           </View>
           {/* <InputWithLabel
             label="Mobile "
