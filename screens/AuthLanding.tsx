0bb76cffe00667a49bd0fa9800e7272afc0ec1be
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-04-30 22:54:41 +0530

    style: refine layout and spacing in SignInScreen for enhanced readability and user experience

---
+++ screens/AuthLanding.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { View } from "react-native";
+
+const AuthLanding = () => {
+  return <View className="bg-white h-full w-full"></View>;
+};
+
+export default AuthLanding;
