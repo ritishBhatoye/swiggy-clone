f9b8b5558e6791b3e44c142e4514c16487e5461f
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-04-30 22:52:20 +0530

    style: reorganize layout in SignInScreen for improved structure and readability

--- screens/SignIn.tsx
+++ screens/SignIn.tsx
@@ -13,38 +13,40 @@ const SignInScreen = () => {
   const [mobile, setMobile] = useState("");
 
   return (
-    <View className="flex flex-col bg-white items-center w-11/12 mx-auto gap-4">
-      <Text className="text-2xl text-black font-semibold text-start w-full">
-        ACCOUNT
-      </Text>
-      <Text className="text-base text-gray-400 font-medium w-full">
-        Login/Create Account to manage order
-      </Text>
-
-      <Button
-        title={"LOGIN"}
-        textClassName="px-12 "
-        className="rounded-sm my-2 py-4 mt-3 w-full"
-      />
-      <Text className="w-full text-sm  font-medium text-gray-400">
-        By clicking, I accept the{" "}
-        <Text className="text-black font-semibold">
-          Terms & Conditions & Privacy
+    <View className="bg-white h-full w-full">
+      <View className="flex flex-col items-center w-11/12 mx-auto gap-4">
+        <Text className="text-2xl text-black font-semibold text-start w-full">
+          ACCOUNT
         </Text>
-      </Text>
-      <View className="my-5 shadow-none border-b border-black w-full" />
-      <ListTile
-        className="border rounded-none border-dashed border-gray-500"
-        title={"Send Feedback"}
-        leading={"mail-outline"}
-        color=" #6b7280"
-      />
-      <ListTile
-        className="border my-3 rounded-none  border-dashed border-gray-500"
-        title={"Send Feedback"}
-        leading={"mail-outline"}
-        color=" #6b7280"
-      />
+        <Text className="text-base text-gray-400 font-medium w-full">
+          Login/Create Account to manage order
+        </Text>
+
+        <Button
+          title={"LOGIN"}
+          textClassName="px-12 "
+          className="rounded-sm my-2 py-4 mt-3 w-full"
+        />
+        <Text className="w-full text-sm  font-medium text-gray-400">
+          By clicking, I accept the{" "}
+          <Text className="text-black font-semibold">
+            Terms & Conditions & Privacy
+          </Text>
+        </Text>
+        <View className="my-5 shadow-none border-b border-black w-full" />
+        <ListTile
+          className="border rounded-none border-dashed border-gray-500"
+          title={"Send Feedback"}
+          leading={"mail-outline"}
+          color=" #6b7280"
+        />
+        <ListTile
+          className="border my-3 rounded-none  border-dashed border-gray-500"
+          title={"Send Feedback"}
+          leading={"mail-outline"}
+          color=" #6b7280"
+        />
+      </View>
     </View>
   );
 };
