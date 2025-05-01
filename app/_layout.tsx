d8329bebeec402e088916c17cec45a6e66a7574a
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-05-01 19:42:30 +0530

    feat: add new screen for (screens) in RootLayout to enhance navigation structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -41,6 +41,7 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(screens)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
