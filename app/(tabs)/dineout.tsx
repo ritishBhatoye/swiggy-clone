593f8326c6de6915725fab5a305bf9667fcfe397
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-06-12 22:44:08 +0530

    chore: update expo-linear-gradient to version 14.1.5 in package.json and package-lock.json; enhance RestaurantCard component styling and layout for improved visual appeal

--- app/(tabs)/dineout.tsx
+++ app/(tabs)/dineout.tsx
@@ -8,11 +8,12 @@ import { FlatList, SafeAreaView, View } from "react-native";
 const DineoutScreen = () => {
   return (
     <SafeAreaView className="bg-white h-full  ">
-      <View className="w-11/12 mx-auto px-2">
+      <View className="w-11/12 mx-auto px-2 py-3 bg-white">
         <TopMenuBar />
       </View>
       <FlatList
         data={restaurantData}
+        contentContainerClassName="px-4 gap-10"
         keyExtractor={(item: any) => item.id}
         renderItem={({ item }) => <RestaurantCard restaurant={item} />}
       />
--- components/elements/Cards/RestaurantCard.tsx
+++ components/elements/Cards/RestaurantCard.tsx
@@ -1,5 +1,6 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Image } from "expo-image";
+import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
 import { TouchableOpacity, View, Text } from "react-native";
 
@@ -9,27 +10,56 @@ interface props {
 
 const RestaurantCard = ({ restaurant }: props) => {
   return (
-    <TouchableOpacity className="rounded-2xl">
-      <Image source={{ uri: restaurant.image }} />
-      <View className="gap-4">
-        <View className="items-center justify-between flex-row">
-          <Text className="text-lg font-bold">{restaurant.name}</Text>
-          <Text className="text-sm font-semibold">{restaurant.rating}</Text>
-        </View>
-        <Text className="text-gray-400 text-sm font-light">
-          {" "}
-          {restaurant.location}
-        </Text>
-        <Text className="text-gray-400 text-sm font-light">
-          {restaurant.popularDish}
-        </Text>
-        {restaurant.offers.map((offer: string) => (
-          <View className="flex-row gap-1 items-center">
-            <Ionicons name="pricetag-outline" color={"primary"} size={20} />
-            <Text className="text-gray-400 text-sm font-light">{offer}</Text>
+    <TouchableOpacity className="rounded-xl border-[0.4px] border-gray-400 ">
+      <Image
+        className="w-full"
+        source={{ uri: restaurant.image }}
+        contentFit="fill"
+        style={{
+          width: "100%",
+          height: 240,
+          borderTopLeftRadius: 12,
+          borderTopRightRadius: 12,
+        }}
+      />
+      <LinearGradient
+        colors={["#FFF7F0", "#FFB476"]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 2 }}
+        style={{ flex: 1, borderRadius: 12 }}
+      >
+        <View className="gap-1.5 px-5 py-2">
+          <View className="items-center justify-between flex-row">
+            <Text className="text-2xl font-extrabold">{restaurant.name}</Text>
+            <View className="gap-1 flex-row items-center">
+              <Ionicons
+                name="star"
+                size={13}
+                color={"white"}
+                className="p-1 bg-green-700 rounded-full"
+              />
+              <Text className="text-lg font-semibold">{restaurant.rating}</Text>
+            </View>
           </View>
-        ))}
-      </View>
+          <Text className="text-gray-400 text-sm font-light">
+            {restaurant.location}
+          </Text>
+          <Text className="text-gray-400 text-sm font-light">
+            {restaurant.popularDish}
+          </Text>
+          <View className="border-b-[0.3px]" />
+          {restaurant.offers.map((offer: string, id: number) => (
+            <View className="flex-row gap-2 items-center" key={id}>
+              <Ionicons
+                name={id == 1 ? "star" : "pricetag-outline"}
+                color={"#EF4F27"}
+                size={20}
+              />
+              <Text className="text-gray-400 text-sm font-light">{offer}</Text>
+            </View>
+          ))}
+        </View>
+      </LinearGradient>
     </TouchableOpacity>
   );
 };
