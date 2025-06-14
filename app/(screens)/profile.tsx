24ea3ecc02a4a8c3e94a5f50769aa2a40a98a39d
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-06-15 00:30:14 +0530

    feat: refactor Profile screen to use Animated.ScrollView for smoother scrolling and dynamic header title adjustment

--- app/(screens)/profile.tsx
+++ app/(screens)/profile.tsx
@@ -2,43 +2,67 @@ import ActionTile from "@/components/atoms/ActionTile";
 import ProfileCard from "@/components/settings/ProfileCard";
 import { SettingTileData } from "@/constants/dummyData/settings";
 import { Ionicons } from "@expo/vector-icons";
-import React from "react";
-
+import React, { useEffect, useRef } from "react";
 import {
   View,
   Text,
   SafeAreaView,
   FlatList,
   TouchableOpacity,
-  ScrollView,
+  Animated,
 } from "react-native";
+import { useNavigation } from "expo-router";
 
 const Profile = () => {
+  const scrollY = useRef(new Animated.Value(0)).current;
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const listener = scrollY.addListener(({ value }) => {
+      if (value > 100) {
+        navigation.setOptions({ headerTitle: "My Account" });
+      } else {
+        navigation.setOptions({ headerTitle: "" });
+      }
+    });
+
+    return () => {
+      scrollY.removeListener(listener);
+    };
+  }, []);
+
   return (
     <SafeAreaView className="flex-1">
-      <ProfileCard />
-      <ScrollView>
-        <FlatList
-          contentContainerClassName={`px-5 gap-4  bg-white`}
-          keyExtractor={(item) => item.id.toString()}
-          data={SettingTileData}
-          renderItem={({ item, index }: { item: any; index: number }) => (
+      <Animated.ScrollView
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+          { useNativeDriver: false }
+        )}
+        scrollEventThrottle={16}
+      >
+        <ProfileCard />
+
+        {/* FlatList won't work directly inside ScrollView. So render manually */}
+        <View className="px-5 bg-white">
+          {SettingTileData.map((item, index) => (
             <View
+              key={item.id}
               className={`${
                 index === SettingTileData.length - 1 ? "" : "border-b-[0.2px]"
-              }  py-5`}
+              } py-5`}
             >
               <ActionTile actionData={item} />
             </View>
-          )}
-        />
+          ))}
+        </View>
+
         <TouchableOpacity className="p-5 bg-white flex flex-row justify-between my-4">
           <Text className="text-black text-start text-lg font-semibold">
             LOGOUT
           </Text>
           <Ionicons name="chevron-forward-outline" color={"black"} size={24} />
         </TouchableOpacity>
-      </ScrollView>
+      </Animated.ScrollView>
     </SafeAreaView>
   );
 };
