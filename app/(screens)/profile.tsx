865b941c8090a320d655c8f0df1561695bef817f
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-06-15 00:20:11 +0530

    feat: refactor Profile screen layout to use ScrollView and enhance logout button with icon for improved user experience

--- app/(screens)/profile.tsx
+++ app/(screens)/profile.tsx
@@ -1,6 +1,7 @@
 import ActionTile from "@/components/atoms/ActionTile";
 import ProfileCard from "@/components/settings/ProfileCard";
 import { SettingTileData } from "@/constants/dummyData/settings";
+import { Ionicons } from "@expo/vector-icons";
 import React from "react";
 
 import {
@@ -9,27 +10,31 @@ import {
   SafeAreaView,
   FlatList,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 
 const Profile = () => {
   return (
-    <SafeAreaView className="flex">
+    <SafeAreaView className="flex-1">
       <ProfileCard />
-      <FlatList
-        contentContainerClassName="px-5 gap-4"
-        keyExtractor={(item) => item.id.toString()}
-        data={SettingTileData}
-        renderItem={({ item }: { item: any }) => (
-          <View className="border-b-[0.2px] py-5">
-            <ActionTile actionData={item} />
-          </View>
-        )}
-      />
-      <TouchableOpacity className="py-4 px-5 bg-white">
-        <Text className="text-black text-start text-lg font-semibold">
-          LOGOUT
-        </Text>
-      </TouchableOpacity>
+      <ScrollView>
+        <FlatList
+          contentContainerClassName="px-5 gap-4  bg-white"
+          keyExtractor={(item) => item.id.toString()}
+          data={SettingTileData}
+          renderItem={({ item }: { item: any }) => (
+            <View className="border-b-[0.2px] py-5">
+              <ActionTile actionData={item} />
+            </View>
+          )}
+        />
+        <TouchableOpacity className="py-4 px-5 bg-white flex flex-row justify-between my-4">
+          <Text className="text-black text-start text-lg font-semibold">
+            LOGOUT
+          </Text>
+          <Ionicons name="chevron-forward-outline" color={"black"} size={24} />
+        </TouchableOpacity>
+      </ScrollView>
     </SafeAreaView>
   );
 };
