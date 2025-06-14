81382424cf24c671ea1ab1c4735e5099630f9565
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-06-14 21:15:24 +0530

    feat: enhance ReorderCard with improved layout and dynamic rendering of restaurant details and ordered items

---
+++ constants/dummyData/reorder.ts
@@ -0,0 +1,262 @@
+export const reorderData: ReorderCardDataType[] = [
+  {
+    restaurant: {
+      name: "Punjabi Dhaba",
+      image: "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4",
+      deliveryTime: "25-30 min",
+    },
+    cheapDishPrice: "₹99",
+    itemsOrdered: {
+      dish: "Butter Chicken",
+      price: "₹299",
+      isVeg: false,
+    },
+  },
+  {
+    restaurant: {
+      name: "South Spice",
+      image: "https://images.unsplash.com/photo-1552566626-52f8b828add9",
+      deliveryTime: "20-25 min",
+    },
+    cheapDishPrice: "₹79",
+    itemsOrdered: {
+      dish: "Masala Dosa",
+      price: "₹149",
+      isVeg: true,
+    },
+  },
+  {
+    restaurant: {
+      name: "Biryani House",
+      image: "https://images.unsplash.com/photo-1563379091339-03b21ab4a4f8",
+      deliveryTime: "30-35 min",
+    },
+    cheapDishPrice: "₹129",
+    itemsOrdered: {
+      dish: "Chicken Biryani",
+      price: "₹249",
+      isVeg: false,
+    },
+  },
+  {
+    restaurant: {
+      name: "Pizza Paradise",
+      image: "https://images.unsplash.com/photo-1513104890138-7c749659a591",
+      deliveryTime: "25-30 min",
+    },
+    cheapDishPrice: "₹149",
+    itemsOrdered: {
+      dish: "Margherita Pizza",
+      price: "₹299",
+      isVeg: true,
+    },
+  },
+  {
+    restaurant: {
+      name: "Burger King",
+      image: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd",
+      deliveryTime: "15-20 min",
+    },
+    cheapDishPrice: "₹99",
+    itemsOrdered: {
+      dish: "Whopper",
+      price: "₹199",
+      isVeg: false,
+    },
+  },
+  {
+    restaurant: {
+      name: "Chinese Wok",
+      image: "https://images.unsplash.com/photo-1563245372-f21724e3856d",
+      deliveryTime: "20-25 min",
+    },
+    cheapDishPrice: "₹129",
+    itemsOrdered: {
+      dish: "Noodles",
+      price: "₹179",
+      isVeg: true,
+    },
+  },
+  {
+    restaurant: {
+      name: "Ice Cream Delight",
+      image: "https://images.unsplash.com/photo-1563805042-7684c019e1cb",
+      deliveryTime: "15-20 min",
+    },
+    cheapDishPrice: "₹49",
+    itemsOrdered: {
+      dish: "Chocolate Sundae",
+      price: "₹99",
+      isVeg: true,
+    },
+  },
+  {
+    restaurant: {
+      name: "Tandoori Nights",
+      image: "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4",
+      deliveryTime: "30-35 min",
+    },
+    cheapDishPrice: "₹149",
+    itemsOrdered: {
+      dish: "Tandoori Chicken",
+      price: "₹399",
+      isVeg: false,
+    },
+  },
+  {
+    restaurant: {
+      name: "Sushi Master",
+      image: "https://images.unsplash.com/photo-1579871494447-9811cf80d66c",
+      deliveryTime: "25-30 min",
+    },
+    cheapDishPrice: "₹199",
+    itemsOrdered: {
+      dish: "California Roll",
+      price: "₹299",
+      isVeg: false,
+    },
+  },
+  {
+    restaurant: {
+      name: "Cafe Coffee Day",
+      image: "https://images.unsplash.com/photo-1495474472287-4d71bcdd2085",
+      deliveryTime: "15-20 min",
+    },
+    cheapDishPrice: "₹79",
+    itemsOrdered: {
+      dish: "Cappuccino",
+      price: "₹129",
+      isVeg: true,
+    },
+  },
+  {
+    restaurant: {
+      name: "Subway",
+      image: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd",
+      deliveryTime: "20-25 min",
+    },
+    cheapDishPrice: "₹99",
+    itemsOrdered: {
+      dish: "Veg Sub",
+      price: "₹149",
+      isVeg: true,
+    },
+  },
+  {
+    restaurant: {
+      name: "Dominos",
+      image: "https://images.unsplash.com/photo-1513104890138-7c749659a591",
+      deliveryTime: "25-30 min",
+    },
+    cheapDishPrice: "₹129",
+    itemsOrdered: {
+      dish: "Pepperoni Pizza",
+      price: "₹299",
+      isVeg: false,
+    },
+  },
+  {
+    restaurant: {
+      name: "KFC",
+      image: "https://images.unsplash.com/photo-1562967914-608f82629710",
+      deliveryTime: "20-25 min",
+    },
+    cheapDishPrice: "₹99",
+    itemsOrdered: {
+      dish: "Chicken Bucket",
+      price: "₹499",
+      isVeg: false,
+    },
+  },
+  {
+    restaurant: {
+      name: "McDonald's",
+      image: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd",
+      deliveryTime: "15-20 min",
+    },
+    cheapDishPrice: "₹49",
+    itemsOrdered: {
+      dish: "Big Mac",
+      price: "₹199",
+      isVeg: false,
+    },
+  },
+  {
+    restaurant: {
+      name: "Haldiram's",
+      image: "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4",
+      deliveryTime: "20-25 min",
+    },
+    cheapDishPrice: "₹79",
+    itemsOrdered: {
+      dish: "Chole Bhature",
+      price: "₹149",
+      isVeg: true,
+    },
+  },
+  {
+    restaurant: {
+      name: "Baskin Robbins",
+      image: "https://images.unsplash.com/photo-1563805042-7684c019e1cb",
+      deliveryTime: "15-20 min",
+    },
+    cheapDishPrice: "₹99",
+    itemsOrdered: {
+      dish: "Ice Cream Cake",
+      price: "₹399",
+      isVeg: true,
+    },
+  },
+  {
+    restaurant: {
+      name: "Pizza Hut",
+      image: "https://images.unsplash.com/photo-1513104890138-7c749659a591",
+      deliveryTime: "25-30 min",
+    },
+    cheapDishPrice: "₹149",
+    itemsOrdered: {
+      dish: "Cheese Pizza",
+      price: "₹299",
+      isVeg: true,
+    },
+  },
+  {
+    restaurant: {
+      name: "Starbucks",
+      image: "https://images.unsplash.com/photo-1495474472287-4d71bcdd2085",
+      deliveryTime: "20-25 min",
+    },
+    cheapDishPrice: "₹129",
+    itemsOrdered: {
+      dish: "Caramel Frappuccino",
+      price: "₹249",
+      isVeg: true,
+    },
+  },
+  {
+    restaurant: {
+      name: "Subway",
+      image: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd",
+      deliveryTime: "20-25 min",
+    },
+    cheapDishPrice: "₹99",
+    itemsOrdered: {
+      dish: "Chicken Sub",
+      price: "₹199",
+      isVeg: false,
+    },
+  },
+  {
+    restaurant: {
+      name: "Costa Coffee",
+      image: "https://images.unsplash.com/photo-1495474472287-4d71bcdd2085",
+      deliveryTime: "15-20 min",
+    },
+    cheapDishPrice: "₹89",
+    itemsOrdered: {
+      dish: "Latte",
+      price: "₹149",
+      isVeg: true,
+    },
+  },
+];
